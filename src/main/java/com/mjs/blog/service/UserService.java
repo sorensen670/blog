92c4712e40fdde47244a3fda463dfe3e2d0f4f1e
Michael Sorensen <michaelsorensen@Yggdrasil.local>
2025-05-04 12:51:53 -0500

    UserService updates

--- src/main/java/com/mjs/blog/service/UserService.java
+++ src/main/java/com/mjs/blog/service/UserService.java
@@ -1,4 +1,27 @@
 package com.mjs.blog.service;
 
+import com.mjs.blog.model.User;
+import com.mjs.blog.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+
 }
